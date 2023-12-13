bd65352e8113b39fcfd7da94c34fd856fffd668f
chaitu-1998 <cvidudha@uab.edu>
2023-12-12 18:00:51 -0600

    Adding index to console to know the index value

---
+++ messageGenarator.js
@@ -0,0 +1,35 @@
+//creating any array to hold the jokes 
+const jokesQuestion = ["Why don't scientists trust atoms?",
+"What did one wall say to the other wall?",
+"Why did the scarecrow win an award?",
+"How do you organize a space party?",
+"Why don't skeletons fight each other?",
+"What do you call fake spaghetti?",
+"Why did the bicycle fall over?",
+"Did you hear about the mathematician who’s afraid of negative numbers?",
+"What did one ocean say to the other ocean?",
+"Why did the tomato turn red?"];
+const jokeAnswer = ["Because they make up everything!",
+"I'll meet you at the corner.",
+"Because he was outstanding in his field!",
+"You planet!",
+"They don't have the guts.",
+"An impasta.",
+"Because it was two-tired!",
+"He will stop at nothing to avoid them.",
+"Nothing, they just waved.",
+"Because it saw the salad dressing!"];
+
+//to pick a random joke question
+function pickAQuestion(arr)
+{
+    let jokeQesIndex = Math.floor(Math.random()*(arr.length-1))
+    return jokeQesIndex
+}
+
+let indexOfArray = pickAQuestion(jokesQuestion)
+
+
+console.log(jokesQuestion[indexOfArray])
+
+console.log(jokeAnswer[indexOfArray])
